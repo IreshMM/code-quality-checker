cad2cf498d64e1e049146c035a403f9f0bfc4e3d
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-04-26 11:18:51 +0530

    automatic job creation implementation for jenkins

--- src/jenkinsapi.ts
+++ src/jenkinsapi.ts
@@ -1,13 +1,16 @@
 import jenkins from "jenkins";
+import fs from "fs";
 
 const JENKINS_HOST = process.env.JENKINS_HOST;
 const JENKINS_PORT = process.env.JENKINS_PORT;
 const JENKINS_LOGIN = process.env.JENKINS_LOGIN;
 const JENKINS_PASS = process.env.JENKINS_PASS;
-const JENKINS_PROJECT = process.env.JENKINS_PROJECT;
+const JENKINS_PROJECT = process.env.JENKINS_PROJECT || "JavaCodeQualityChecker";
 const JENKINS_SUBPATH = process.env.JENKINS_SUBPATH;
 
-const JENKINS_ACCESS_URL = `http://${JENKINS_LOGIN}:${JENKINS_PASS}@${JENKINS_HOST}:${JENKINS_PORT}/${JENKINS_SUBPATH ? JENKINS_SUBPATH : ''}`;
+const JENKINS_ACCESS_URL = `http://${JENKINS_LOGIN}:${JENKINS_PASS}@${JENKINS_HOST}:${JENKINS_PORT}/${
+  JENKINS_SUBPATH ? JENKINS_SUBPATH : ""
+}`;
 
 const jenkinsInstance = jenkins({
   baseUrl: JENKINS_ACCESS_URL,
@@ -20,15 +23,51 @@ export function startSonarQubeScanViaJenkins(
   sonarProjectKey: string,
   callBack?: (err: Error, data: any) => void
 ) {
-  jenkinsInstance.job.build(
-    {
-      name: `${JENKINS_PROJECT}`,
-      parameters: {
-        gitHubRepoUrl,
-        branch,
-        sonarProjectKey,
+  ensureJobExistsAndContinue(JENKINS_PROJECT, () => {
+    jenkinsInstance.job.build(
+      {
+        name: JENKINS_PROJECT,
+        parameters: {
+          gitHubRepoUrl,
+          branch,
+          sonarProjectKey,
+        },
       },
-    },
-    callBack ? callBack : console.log
-  );
+      callBack ? callBack : console.log
+    );
+  });
+}
+
+export function ensureJobExistsAndContinue(
+  projectName: string,
+  continueCallBack: () => void
+) {
+  console.log('ensureJobExistsAndContinueExecution');
+  
+  jenkinsInstance.job.exists(projectName, (error, exists) => {
+    if (!exists && !error) {
+      console.log('JobDoesNotExist');
+      
+      createJob(projectName, (created) => {
+        if (created) {
+          continueCallBack();
+        }
+      });
+    } else {
+      continueCallBack();
+    }
+  });
+}
+
+function createJob(projectName: string, callBack: (created: Boolean) => void) {
+  fs.readFile("./jobconfig/job.xml", "utf8", (err, data) => {
+    if (!err) {
+      jenkinsInstance.job.create(projectName, data, (err) => {
+        if (!err) {
+          console.log("job created");
+          callBack(true);
+        }
+      });
+    }
+  });
 }
