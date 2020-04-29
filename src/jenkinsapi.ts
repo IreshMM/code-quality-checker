9f61b5fc5077723be81e304a924ed3432dfaf49b
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-04-29 15:20:13 +0530

    implement a general startJenkinsJob startSonarQubePRAnalyzisViaJenkins function and required config files

--- src/jenkinsapi.ts
+++ src/jenkinsapi.ts
@@ -1,14 +1,18 @@
 import jenkins from "jenkins";
 import fs from "fs";
 
-const JENKINS_HOST = process.env.JENKINS_HOST;
-const JENKINS_PORT = process.env.JENKINS_PORT;
+// Jenkins access information
 const JENKINS_LOGIN = process.env.JENKINS_LOGIN;
 const JENKINS_PASS = process.env.JENKINS_PASS;
-const JENKINS_PROJECT = process.env.JENKINS_PROJECT || "JavaCodeQualityChecker";
+const JENKINS_HOST = process.env.JENKINS_HOST;
+const JENKINS_PORT = process.env.JENKINS_PORT;
 const JENKINS_SUBPATH = process.env.JENKINS_SUBPATH;
-const JENKINS_CONFIG_COMMIT_STATUSES = process.env.JENKINS_CONFIG_COMMIT_STATUSES || 'job.xml';
-const JENKINS_CONFIG_PULL_REQUEST = process.env.JENKINS_CONFIG_PULL_REQUEST;
+
+// Jenkins job information
+const JENKINS_PROJECT_QUALITY_CHECK = process.env.JENKINS_PROJECT_QUALITY_CHECK!;
+const JENKINS_PROJECT_PR_CHECK = process.env.JENKINS_PROJECT_PR_CHECK!;
+const JENKINS_CONFIG_COMMIT_STATUSES = process.env.JENKINS_CONFIG_COMMIT_STATUSES!;
+const JENKINS_CONFIG_PULL_REQUEST = process.env.JENKINS_CONFIG_PULL_REQUEST!;
 
 const JENKINS_ACCESS_URL = `http://${JENKINS_LOGIN}:${JENKINS_PASS}@${JENKINS_HOST}:${JENKINS_PORT}/${
   JENKINS_SUBPATH ? JENKINS_SUBPATH : ""
@@ -19,21 +23,57 @@ const jenkinsInstance = jenkins({
   crumbIssuer: true,
 });
 
+export function startSonarQubePRAnalyzisViaJenkins(
+  gitHubRepoUrl: string,
+  baseBranch: string,
+  headBranch: string,
+  pullNumber: string,
+  sonarProjectKey: string,
+  callBack?: (err: Error, data: any) => void
+) {
+  startJenkinsJob(
+    JENKINS_CONFIG_PULL_REQUEST,
+    JENKINS_PROJECT_PR_CHECK,
+    {
+      gitHubRepoUrl,
+      baseBranch,
+      headBranch,
+      pullNumber,
+      sonarProjectKey,
+    },
+    callBack
+  );
+}
+
 export function startSonarQubeScanViaJenkins(
   gitHubRepoUrl: string,
   branch: string,
   sonarProjectKey: string,
   callBack?: (err: Error, data: any) => void
 ) {
-  ensureJobExistsAndContinue(JENKINS_CONFIG_COMMIT_STATUSES, JENKINS_PROJECT, () => {
+  startJenkinsJob(
+    JENKINS_CONFIG_COMMIT_STATUSES,
+    JENKINS_PROJECT_QUALITY_CHECK,
+    {
+      gitHubRepoUrl,
+      branch,
+      sonarProjectKey,
+    },
+    callBack
+  );
+}
+
+export function startJenkinsJob(
+  configFile: string,
+  job: string,
+  parameters: any,
+  callBack?: (err: Error, data: any) => void
+) {
+  ensureJobExistsAndContinue(configFile, job, () => {
     jenkinsInstance.job.build(
       {
-        name: JENKINS_PROJECT,
-        parameters: {
-          gitHubRepoUrl,
-          branch,
-          sonarProjectKey,
-        },
+        name: job,
+        parameters: parameters,
       },
       callBack ? callBack : console.log
     );
@@ -45,12 +85,12 @@ export function ensureJobExistsAndContinue(
   projectName: string,
   continueCallBack: () => void
 ) {
-  console.log('ensureJobExistsAndContinueExecution');
-  
+  console.log("ensureJobExistsAndContinueExecution");
+
   jenkinsInstance.job.exists(projectName, (error, exists) => {
     if (!exists && !error) {
-      console.log('JobDoesNotExist');
-      
+      console.log("JobDoesNotExist");
+
       createJob(jobConfigFile, projectName, (created) => {
         if (created) {
           continueCallBack();
@@ -62,7 +102,11 @@ export function ensureJobExistsAndContinue(
   });
 }
 
-function createJob(jobConfigFile: string, projectName: string, callBack: (created: Boolean) => void) {
+function createJob(
+  jobConfigFile: string,
+  projectName: string,
+  callBack: (created: Boolean) => void
+) {
   fs.readFile(`./jobconfig/${jobConfigFile}`, "utf8", (err, data) => {
     if (!err) {
       jenkinsInstance.job.create(projectName, data, (err) => {
