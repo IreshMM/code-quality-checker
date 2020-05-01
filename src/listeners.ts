7313f6fcc566772a83c2eb5a85d7a1e3dd9f9c9c
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-05-01 11:33:28 +0530

    implement pull request listener + fix filename of commitstatusjob.xml

--- src/listeners.ts
+++ src/listeners.ts
@@ -17,23 +17,22 @@ export async function onPush(context: Context<Webhooks.WebhookPayloadPush>) {
 }
 
 export async function onPullRequest(context: Context<Webhooks.WebhookPayloadPullRequest>) {
-  console.log(context.payload.pull_request.title);
+  console.log("PullRequestCreationDetected");
+  utils.determineProjectType(context).then((projectType: utils.ProjectType) => {
+    if(projectType == utils.ProjectType.JAVA) {
+      console.log("InsideProjectTypeJavaFromPullRequest");
+
+      utils.startSonarQubePRAnalyzis(context);
+    }
+  })
+
 
   
-  setTimeout(closePullRequest.bind(null, context), 5000);
+  // setTimeout(closePullRequest.bind(null, context), 5000);
 }
 
 export function onSonarQubeWebhook(req: Request, res: Response) {
   utils.updateQualityGateStatus(req.body);
   utils.cleanWorkspace(req.body.revision);
   res.sendStatus(200);
-}
-
-function closePullRequest(context: Context<Webhooks.WebhookPayloadPullRequest>) {
-  // Close the pull request to make the testing easier
-  const params : {state: 'open' | 'closed'} = {
-    state: 'closed'
-  }
-  const pullRequest = context.issue(params);
-  context.github.pulls.update(pullRequest);
 }
