74ce7589a3a0d15f3559c8bc62576f5b9cd935b9
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-05-01 11:23:31 +0530

    fix and add some getters

--- src/getters.ts
+++ src/getters.ts
@@ -4,24 +4,23 @@ import {
 } from "@octokit/webhooks";
 
 export function getCommitSha(payload: any): string {
-  return payload.head_commit.id;
+  if(payload.head_commit) return payload.head_commit.id;
+  const typedPayload: WebhookPayloadPullRequest = payload;
+  return typedPayload.pull_request.head.sha;
 }
 
 export function getBranch(
   payload: WebhookPayloadPush | WebhookPayloadPullRequest
 ) {
-  const ref = getRef(payload);
-  return ref.split("/")[2];
+  if ("ref" in payload) {
+    const ref = getRef(payload);
+    return ref.split("/")[2];
+  }
+  return getHeadBranch(payload);
 }
 
-export function getRef(
-  payload: WebhookPayloadPush | WebhookPayloadPullRequest
-) {
-  if ("ref" in payload) {
-    return payload.ref;
-  } else {
-    return payload.pull_request.head.ref;
-  }
+export function getRef(payload: WebhookPayloadPush) {
+  return payload.ref;
 }
 
 export function getCloneUrl(
@@ -39,5 +38,28 @@ export function getRepoName(
 export function getOwner(
   payload: WebhookPayloadPush | WebhookPayloadPullRequest
 ) {
-  return payload.repository.owner.name;
+  if(payload.repository.owner.name) return payload.repository.owner.name;
+  return payload.repository.owner.login;
+}
+
+export function getHeadRef(payload: WebhookPayloadPullRequest) {
+  return payload.pull_request.head.ref;
+}
+
+export function getBaseRef(payload: WebhookPayloadPullRequest) {
+  return payload.pull_request.base.ref;
+}
+
+export function getHeadBranch(payload: WebhookPayloadPullRequest) {
+  const ref = getHeadRef(payload);
+  return ref;
+}
+
+export function getBaseBranch(payload: WebhookPayloadPullRequest) {
+  const ref = getBaseRef(payload);
+  return ref;
+}
+
+export function getPullRequestNumber(payload: WebhookPayloadPullRequest) {
+  return payload.pull_request.number.toString();
 }
