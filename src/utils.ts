b9528bf64fcdc9b1dc2eccad98137e8623b678e4
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-05-01 11:31:17 +0530

    reimplement determineProjectTypeEfficiently + removal of unnecassary dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -3,19 +3,14 @@ import Webhooks from "@octokit/webhooks";
 import rimraf from "rimraf";
 import * as getters from "./getters";
 import { EventEmitter } from "events";
-import simpleGit from "simple-git/promise";
 import dotenv from "dotenv";
 import fs from "fs";
-import { exec } from "child_process";
-import { promisify } from "util";
 import { ensureProjectExists } from "./sonarapi";
-import { startSonarQubeScanViaJenkins } from "./jenkinsapi";
-const promisifiedExec = promisify(exec);
+import { startSonarQubeScanViaJenkins, startSonarQubePRAnalyzisViaJenkins } from "./jenkinsapi";
 dotenv.config();
 
 const GIT_WORKSPACE = `${process.env.HOME}/${process.env.WORKSPACE}`;
 createGitWorkspace();
-const gitWorkspace = simpleGit(GIT_WORKSPACE);
 const qualityGateEventEmitter = new EventEmitter();
 
 export enum ProjectType {
@@ -24,35 +19,15 @@ export enum ProjectType {
 }
 
 export async function determineProjectType(
-  context: Context<Webhooks.WebhookPayloadPush> | Context<Webhooks.WebhookPayloadPullRequest>
+  context:
+    | Context<Webhooks.WebhookPayloadPush>
+    | Context<Webhooks.WebhookPayloadPullRequest>
 ): Promise<ProjectType> {
-  if (context) {
-    const repoName = getters.getRepoName(context.payload);
-    if (repoName.toLocaleLowerCase().includes("java")) {
-      return ProjectType.JAVA;
-    }
-
-    await cleanWorkspace(getters.getCommitSha(context.payload));
-    await cloneCommit(context.payload);
-
-    const gitRepoPath = `${GIT_WORKSPACE}/${getters.getCommitSha(
-      context.payload
-    )}`;
-    try {
-      const { stdout, stderr } = await promisifiedExec(
-        `bash lib/determineprojecttype.sh ${gitRepoPath}`
-      );
-      if (stdout.includes("JAVA")) {
-        console.log("Project type java identified");
-
-        return ProjectType.JAVA;
-      }
-      console.log(stderr);
-    } catch (error) {
-      console.log(error);
-    }
+  const lang = context.payload.repository.language;
+  
+  if(lang?.toLowerCase().includes('java')) {
+    return ProjectType.JAVA;
   }
-  console.log("ProjectType: NOTJAVA");
 
   return ProjectType.NOTJAVA;
 }
@@ -60,29 +35,6 @@ export async function determineProjectType(
 export function cleanWorkspace(commitSha: string) {
   rimraf.sync(`${GIT_WORKSPACE}/${commitSha}`);
   console.log(`${GIT_WORKSPACE}/${commitSha} is cleaned`);
-}
-
-async function cloneCommit(payload: Webhooks.WebhookPayloadPush | Webhooks.WebhookPayloadPullRequest) {
-  console.log("cloneCommitExecution");
-  const GITHUB_LOGIN = process.env.GITHUB_LOGIN;
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-
-  let remoteUrl;
-  if (GITHUB_LOGIN && GITHUB_TOKEN) {
-    const repoOwner = getters.getOwner(payload);
-    const repoName = getters.getRepoName(payload);
-    remoteUrl = `https://${GITHUB_LOGIN}:${GITHUB_TOKEN}@github.com/${repoOwner}/${repoName}`;
-  } else {
-    remoteUrl = getters.getCloneUrl(payload);
-  }
-
-  const branch = getters.getBranch(payload);
-  const sha = getters.getCommitSha(payload);
-  await gitWorkspace.clone(remoteUrl, sha, [
-    "--single-branch",
-    `--branch=${branch}`,
-    "--depth=1",
-  ]);
 }
 
 export function startSonarQubeScan(
@@ -109,6 +61,37 @@ export function startSonarQubeScan(
           console.log(err);
         }
       );
+    }
+  });
+}
+
+export function startSonarQubePRAnalyzis(
+  context: Context<Webhooks.WebhookPayloadPullRequest>
+) {
+  const projectKey = generateProjectKey(context);
+  const projectName = getters.getRepoName(context.payload);
+  const gitHubRepoUrl = getters.getCloneUrl(context.payload);
+  const baseBranch = getters.getBaseBranch(context.payload);
+  const headBranch = getters.getHeadBranch(context.payload);
+  const pullRequestNumber = getters.getPullRequestNumber(context.payload);
+  
+  console.log("Obtained details");
+  ensureProjectExists(projectKey, projectName).then((success) => {
+    if(success) {
+      startSonarQubePRAnalyzisViaJenkins(
+        gitHubRepoUrl,
+        baseBranch,
+        headBranch,
+        pullRequestNumber,
+        projectKey,
+        (err, data) => {
+          if(!err) {
+            console.log(data);
+            return;
+          }
+          console.log(err);
+        }
+      )
     }
   });
 }
@@ -165,6 +148,10 @@ function createGitWorkspace() {
   }
 }
 
-function generateProjectKey(context: Context<Webhooks.WebhookPayloadPush>) {
+function generateProjectKey(
+  context:
+    | Context<Webhooks.WebhookPayloadPush>
+    | Context<Webhooks.WebhookPayloadPullRequest>
+) {
   return getters.getRepoName(context.payload).trim();
 }
