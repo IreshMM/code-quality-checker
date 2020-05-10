f688e754ad515bfb9b6015133d4f23da6efd389e
Iresh M Dissanayaka <socialexpz1@gmail.com>
2020-05-10 19:51:57 +0530

    sonarqube automated webhook creation function implementation only -- is not being used anywhere yet

--- src/sonarapi.ts
+++ src/sonarapi.ts
@@ -1,8 +1,15 @@
 import fetch from "node-fetch";
 
-const apiBaseUrl = `${process.env.SONARQUBE_URL}/api`;
-const token = process.env.SONAR_LOGIN;
-const encodedToken = new Buffer(token + ":").toString("base64");
+const SONAR_LOGIN = process.env.SONAR_LOGIN;
+const SONARQUBE_URL = process.env.SONARQUBE_URL;
+const BASE_URL = process.env.BASE_URL;
+const SONARQUBE_WEBHOOK_NAME = process.env.SONARQUBE_WEBHOOK_NAME;
+const SONARQUBE_WEBHOOK_SECRET = process.env.SONARQUBE_WEBHOOK_SECRET!;
+
+const apiBaseUrl = `${SONARQUBE_URL}/api`;
+const encodedToken = new Buffer(SONAR_LOGIN + ":").toString("base64");
+const encodedWebhookSecret = new Buffer(SONARQUBE_WEBHOOK_SECRET).toString("base64");
+const encodedWebhookUrl = new Buffer(`${BASE_URL}/webhooks/sonarqube`).toString();
 
 export async function projectExists(projectKey: string) {
   const res = await fetch(
@@ -35,4 +42,45 @@ export async function ensureProjectExists(projectKey: string, name: string) {
     return await createProject(name, projectKey);
   }
   return true;
+}
+
+export async function addWebHookIfNotExists() {
+  if(!await webHookExists()) {
+    await createWebHook();
+  }
+}
+
+async function webHookExists() {
+  const res = await fetch(
+    `${apiBaseUrl}/webhooks/list`,
+    {
+      method: "post",
+      headers: { Authorization: `Basic ${encodedToken}` },
+    }
+  ).then((res) => res.json());
+
+  if(res.webhooks.length > 0) {
+    for (let webhook of res.webhooks) {
+      if(webhook.name == SONARQUBE_WEBHOOK_NAME) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return false;
+}
+
+async function createWebHook() {
+  const res = await fetch(
+    `${apiBaseUrl}/webhooks/create?name=${SONARQUBE_WEBHOOK_NAME}&secret=${encodedWebhookSecret}&url=${encodedWebhookUrl}`,
+    {
+      method: "post",
+      headers: { Authorization: `Basic ${encodedToken}` },
+    }
+  ).then((res) => res.json());
+
+  if(res.webhook) {
+    return true;
+  }
+  return false;
 }
